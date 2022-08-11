b61ba6225ea513383f04aaf093bdb7647d7eb27b
h-spear <80024307+h-spear@users.noreply.github.com>
2022-08-11 15:51:27 +0900

    binary search I 5/12

---
+++ binary-search/find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        upper_bound = 0
+        lower_bound = 0
+
+        if not nums:
+            return [-1, -1]
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] >= target:
+                right = mid - 1
+                lower_bound = mid
+            else:
+                left = mid + 1
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] <= target:
+                left = mid + 1
+                upper_bound = mid
+            else:
+                right = mid - 1
+
+        if nums[upper_bound] != target or nums[lower_bound] != target:
+            return [-1, -1]
+        return [lower_bound, upper_bound]
